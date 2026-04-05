a788d170dc18c9009f21c0b082a7ac189ba6be4f
aleja2701 <alejandra.ortega1@udea.edu.co>
2026-04-05 17:33:05 -0500

    Add KNN use case generator script

---
+++ myquestions/question-0003-usecase-generator.py
@@ -0,0 +1,30 @@
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets import make_classification
+
+def generar_caso_de_uso_evaluar_knn_pesos():
+    X, y = make_classification(
+        n_samples=np.random.randint(50, 150), 
+        n_features=np.random.randint(4, 10), 
+        random_state=np.random.randint(0, 1000)
+    )
+    
+    split_idx = int(len(X) * 0.8)
+    X_train, X_test = X[:split_idx], X[split_idx:]
+    y_train = y[:split_idx]
+    n_vecinos = np.random.randint(3, 8)
+    
+    # Lógica esperada
+    clf = KNeighborsClassifier(n_neighbors=n_vecinos, weights='distance')
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    
+    return {
+        "input": {
+            "X_train": X_train,
+            "y_train": y_train,
+            "X_test": X_test,
+            "n_vecinos": n_vecinos
+        },
+        "output": y_pred
+    }
